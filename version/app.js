b11134b8e254d8f9fd9b9c1b2582e03d7bd0b450
Angelo Gkaraleas <agaraleas@outlook.com>
2023-02-04 09:35:28 +0100

    Create v2 of the application to test build and deployment via openshift

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 1 of the app.' + '\n';
+    response = '<html<b>This is version 2 of the app.</b><br></html>' + '\n';
 
     //send the response to the client
     res.send(response);
