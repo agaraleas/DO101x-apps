f93789159f89c5a42827473c1945355f408423bb
Angelo Gkaraleas <agaraleas@outlook.com>
2023-02-05 19:41:13 +0100

    Revert "Update DB connection configuration to"
    This reverts commit a4389b57c6a42dcea61563e6324df2fea9abb04e.

--- contacts/db/config.js
+++ contacts/db/config.js
@@ -1,17 +1,10 @@
 require('dotenv').config();
-const DB_NAME = process.env.DB_NAME || 'contacts';
-const DB_USER = process.env.DB_USER || 'contacts';
-const DB_PASSWORD = process.env.DB_PASSWORD || 'contacts';
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || '5432';
-const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://' + DB_USER + ':' + DB_PASSWORD + '@' + DB_HOST + ':' + DB_PORT + '/' + DB_NAME;
+const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@localhost:5432/contacts';
 const { Pool } = require('pg');
-
-console.log("Connection string: " + DB_CONFIG);
 
 const pgconn = new Pool({
     connectionString: DB_CONFIG,
     ssl: false,
 });
-
+  
 module.exports = { pgconn }
